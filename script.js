a4c00e52b0383c2b919a861cedc156e1fc6321c2
Deepanshu Chauhan <97678818+deepanshufs@users.noreply.github.com>
2023-04-07 20:20:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".m-text", {
+  strings: ["a Web Developer", "an Ethical Hacker", "a Bug Bounty Hunter"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  backDelay: 500,
+  loop: true
+});
